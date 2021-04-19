a81ad00be7603b686d488c85fcd74fd00b11ea59
muhammad-aji-syabandi <muhammad.aji.syabandi@gmail.com>
2021-04-19 22:18:33 +0800

    Buat Class OperasiString

---
+++ src/string/OperasiString.java
@@ -0,0 +1,12 @@
+package string;
+
+import java.util.Locale;
+
+public class OperasiString {
+    public static void main(String[] args) {
+        String kota = "Matah";
+
+        System.out.println(kota);
+
+    }
+}
