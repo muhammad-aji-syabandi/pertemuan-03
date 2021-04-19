a6d6255df3dfbb86f12adda1713b13a41b07a728
muhammad-aji-syabandi <muhammad.aji.syabandi@gmail.com>
2021-04-19 22:28:59 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,4 +9,13 @@ public class OperatorBinary {
     String nama = "Muhammad Aji Sya'bandi";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.91;
+
+    public static void main(String[] args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
