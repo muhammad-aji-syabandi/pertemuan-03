583bc80515cc6e0bdbf9245d998af0e8b3375c76
muhammad-aji-syabandi <muhammad.aji.syabandi@gmail.com>
2021-04-19 22:27:06 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //operator (-) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Achmad Wira";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.91;
+}
