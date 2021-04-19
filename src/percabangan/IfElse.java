e1ef0a6b96732513597572f235a8df2cee014670
muhammad-aji-syabandi <muhammad.aji.syabandi@gmail.com>
2021-04-19 23:02:04 +0800

    percabangan ifelse condition true

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -3,7 +3,7 @@ package Percabangan;
 public class IfElse {
     public static void main(String[] args){
         double totalBelanja = 50000;
-        double uangDiDompet = 20000;
+        double uangDiDompet = 100000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajanan anda");
