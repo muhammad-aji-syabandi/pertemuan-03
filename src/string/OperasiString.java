fb73bf930824f3c4db2c93a4a7f3a330448d0296
muhammad-aji-syabandi <muhammad.aji.syabandi@gmail.com>
2021-04-19 22:22:18 +0800

    toUppercase untuk ... toLowercase untuk ... substring(5) untuk ... substring(0,4) untuk ...

--- src/string/OperasiString.java
+++ src/string/OperasiString.java
@@ -3,10 +3,18 @@ package string;
 import java.util.Locale;
 
 public class OperasiString {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         String kota = "Matah";
 
         System.out.println(kota);
 
+        char[] uniskaChar = {'u','n','i','s','k','a'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0,4));
     }
 }
